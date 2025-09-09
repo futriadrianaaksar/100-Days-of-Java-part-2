58a1d24e1a9ede43a282c1f6c2adfedb875541ee
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-09 16:24:35 +0800

    Penggunaan println(), print(), dan printf()

---
+++ Day2.java
@@ -0,0 +1,13 @@
+
+public class day2 {
+    public static void main(String[] args) {
+        
+        System.out.print("halo saya ");
+        System.out.print("Ana\n");
+
+        System.out.println("sering di sapa Ana!");
+
+        System.out.printf("Umur saya: %d", 20);
+    }
+}
+  
