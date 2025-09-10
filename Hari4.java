f11c91dff6d08a7ea284a7e21f3adcc19fff3478
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-10 10:48:44 +0800

    Penggunaan Escape Sequence (\n, \t, dan lain lain).

---
+++ Hari4.java
@@ -0,0 +1,19 @@
+public static void main(String[] args) {
+        // Ini adalah tempat program dimulai
+        
+        // Menggunakan \n untuk pindah ke baris baru
+        System.out.println("Halo, nama saya Ana!\nSaya sedang belajar Java.");
+
+        // Menggunakan \t untuk memberikan jarak tab
+        System.out.println("Biodata:\tUmur: 20 tahun");
+
+        // Menggunakan \" untuk mencetak tanda kutip dalam teks
+        System.out.println("Motto: \"Belajar Java itu menyenangkan!\"");
+
+        // Menggunakan \\ untuk mencetak backslash
+        System.out.println("Path folder: C:\\Java\\Latihan");
+
+        // Menggunakan printf dengan \n untuk format rapi
+        System.out.printf("Nama: %s\n\tUmur: %d\n\tHobi: %s\n", "Ana", 20, "Coding");
+    }
+}
