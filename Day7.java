b80f05fca2b65b6cc48fbbddc130025682351bc2
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-11 14:08:45 +0800

    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ Day7.java
@@ -0,0 +1,16 @@
+public class day7 {
+    public static void main(String[] args) {
+       
+        char inisialNama = 'A';
+        boolean sedangBelajar = true;
+
+        System.out.println("Inisial nama Ana: " + inisialNama);
+        System.out.println("Sedang belajar Java? " + sedangBelajar);
+
+        char simbolFavorit = '*';
+        boolean sukaCoding = true;
+
+        System.out.println("Simbol favorit Ana: " + simbolFavorit);
+        System.out.println("Ana suka coding? " + sukaCoding);
+    }
+}
