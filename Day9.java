5c6fa8b5adbdc39876679ebba743b85c0905e1dd
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-11 14:34:30 +0800

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Day9.java
@@ -0,0 +1,20 @@
+public class day9 {
+    public static void main(String[] args) {
+       
+        // Deklarasi dan inisialisasi variabel
+        String nama = "Ana";
+        int umur = 20;
+
+        // Cetak nilai awal
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+
+        // Update variabel
+        nama = "Ana Keren";
+        umur = 21;
+
+        // Cetak nilai setelah update
+        System.out.println("Nama baru: " + nama);
+        System.out.println("Umur baru: " + umur);
+    }
+}
