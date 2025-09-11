79083b8ebe037a57da4935a874de1a7f5c144bd2
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-11 14:03:33 +0800

    Tipe Data Pecahan (double, float).

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class day6 {
+    public static void main(String[] args) {
+        
+        float beratBadan = 55.5f;
+        double tinggiBadan = 165.75;
+
+        System.out.println("Berat badan Ana: " + beratBadan + " kg");
+        System.out.println("Tinggi badan Ana: " + tinggiBadan + " cm");
+    }
+}
