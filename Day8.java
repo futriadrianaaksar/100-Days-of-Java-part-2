7055f355c571b926ef956512e759441dd0bcaa27
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-11 14:25:31 +0800

    Pengenalan Tipe Data String untuk teks.

---
+++ Day8.java
@@ -0,0 +1,13 @@
+public class day8 {
+    public static void main(String[] args) {
+        
+        String nama = "Ana";
+        String hobi = "jalan-jalan";
+
+        System.out.println("Nama saya: " + nama);
+        System.out.println("Hobi saya: " + hobi);
+
+        String pesan = nama + " suka " + hobi;
+        System.out.println(pesan);
+    }
+}
