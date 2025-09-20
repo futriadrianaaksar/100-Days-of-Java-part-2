e6da1ea564dfee83b9ed8e42b64eb23e2b25a246
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-21 06:24:06 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,12 @@
+public class Day14 {
+    public static void main(String[] args) {
+        String a = "111";
+        String b = "16.1";
+
+        int x = Integer.parseInt(a);
+        double y = Double.parseDouble(b);
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
