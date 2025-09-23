a4814430d23f668f2eb6d187abe92d0eb01a7ce5
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-23 19:36:20 +0800

    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ Day15.java
@@ -0,0 +1,10 @@
+public class Day15 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = 5;
+
+        int hasil = a + b - c;
+        System.out.println("Hasil : " + hasil);
+    }
+}
