6f18ca6e8a9d5203fcfbbc0ae2530317252c5a17
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-23 19:47:01 +0800

    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ Day16.java
@@ -0,0 +1,16 @@
+public class Day16 {
+    public static void main(String[] args) {
+        // Contoh perkalian
+        int angka1 = 10;
+        int angka2 = 5;
+        int hasilKali = angka1 * angka2;
+
+        // Contoh pembagian
+        int angka3 = 20;
+        int angka4 = 4;
+        int hasilBagi = angka3 / angka4;
+
+        System.out.println("Hasil perkalian " + angka1 + " * " + angka2 + " = " + hasilKali);
+        System.out.println("Hasil pembagian " + angka3 + " / " + angka4 + " = " + hasilBagi);
+    }
+}
