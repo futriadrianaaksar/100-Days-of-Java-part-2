ca6c4650151527f09a793a960f7efe792964d952
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-24 21:05:47 +0800

    Operator Aritmatika Modulus atau Sisa Bagi (%)

---
+++ Day17.java
@@ -0,0 +1,11 @@
+public class Day17 {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 3;
+
+        // Operator modulus (%)
+        int sisaBagi = angka1 % angka2;
+      
+        System.out.println("Sisa bagi dari " + angka1 + " % " + angka2 + " = " + sisaBagi);
+    }
+                           }
