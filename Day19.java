dcb11900abc19e997c40e5df90dfc49064484d56
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-25 19:47:42 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Day19.java
@@ -0,0 +1,11 @@
+public class Day19 {
+    public static void main(String[] args) {
+     
+        double angkaDouble = 99.99;
+
+        int angkaInt = (int) angkaDouble;
+       
+        System.out.println("Nilai double: " + angkaDouble);
+        System.out.println("Nilai int setelah konversi: " + angkaInt);
+    }
+}
