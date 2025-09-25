fca33da0c66e24c3ce412865045d2e4f9043afe5
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-25 19:37:17 +0800

    Konversi Otomatis (Dari Tipe Data Kecil ke Besar).

---
+++ Day18.java
@@ -0,0 +1,10 @@
+public class Day18 {
+    public static void main(String[] args) {
+        int angkaInt = 100;
+      
+        double angkaDouble = angkaInt;
+
+        System.out.println("Nilai int: " + angkaInt);
+        System.out.println("Nilai double: " + angkaDouble);
+    }
+}
