72e2f89ce448a29867f2e6ee2b9e6aa0761b9fb2
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-28 19:24:55 +0800

    Latihan: Program Luas Persegi.

--- Day21.java
+++ Day21.java
@@ -1,15 +1,9 @@
 public class Day21 {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
+        int sisi = 6;
 
-        System.out.println("Sebelum ditukar: a = " + a + ", b = " + b);
-
-        // proses tukar nilai
-        int temp = a;
-        a = b;
-        b = temp;
-
-        System.out.println("Sesudah ditukar: a = " + a + ", b = " + b);
+        // langsung hitung di dalam println tanpa variabel tambahan
+        System.out.println("Sisi persegi: " + sisi);
+        System.out.println("Luas persegi: " + (sisi * sisi));
     }
 }
