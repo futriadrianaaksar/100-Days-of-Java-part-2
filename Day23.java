00f7c1e122ebfd0c1535de8db0af45df22c54249
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2025-09-30 20:06:05 +0800

    Latihan: Program Luas Persegi Panjang.

---
+++ Day23.java
@@ -0,0 +1,10 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int panjang = 12;
+        int lebar = 6;
+
+        System.out.println("Panjang: " + panjang + " cm");
+        System.out.println("Lebar: " + lebar + " cm");
+        System.out.println("Luas persegi panjang adalah: " + (panjang * lebar) + " cm^2");
+    }
+}
